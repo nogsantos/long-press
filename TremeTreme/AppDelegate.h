d49b9ed92daa43dcc8761a9bb999bf20aba0a2b6
Fabricio Nogueira <nogsantos@gmail.com>
2015-09-16 10:53:13 -0300

    Initial Commit

---
+++ TremeTreme/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TremeTreme
+//
+//  Created by Fabricio Nogueira dos Santos on 9/16/15.
+//  Copyright (c) 2015 Fabricio Nogueira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
